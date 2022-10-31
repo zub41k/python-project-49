7def78f9548ca037f987f32132ac31e513ae0d45
Юрий <zub-69-z@mail.ru>
2022-10-31 09:42:54 +0300

    fix even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,7 +5,7 @@ RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def play_round():
     number = random.randint(1, 100)
-    question = print(f'Question :{number}')
+    question = print(f'Question: {number}')
 
     if number % 2 == 0:
         right_answer: str = 'yes'
