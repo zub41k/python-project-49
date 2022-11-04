1660c4ed995b71c2da8e2f0c8d923cd27a94dbcd
Юрий <zub-69-z@mail.ru>
2022-11-04 13:41:02 +0300

    modified even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -11,7 +11,7 @@ def is_even(number):
 
 def play_round():
     number = random.randint(1, 100)
-    question = f'Question: {number}'
+    question: str = f'Question: {number}'
 
     if is_even(number):
         right_answer: str = 'yes'
