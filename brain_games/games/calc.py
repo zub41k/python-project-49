98f2abf09e5201c5220c7b01601408999d1e9644
Юрий <zub-69-z@mail.ru>
2022-11-10 15:29:37 +0300

    rename func in calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -4,7 +4,7 @@ RULES = 'What is the result of the expression?'
 OPERATORS = ['+', '-', '*']
 
 
-def generate_equation(operator, first_number, second_number):
+def calculate(operator, first_number, second_number):
 
     if operator == '+':
         return first_number + second_number
@@ -19,7 +19,7 @@ def generate_round():
     first_number = random.randint(1, 10)
     second_number = random.randint(1, 10)
 
-    right_answer = generate_equation(operator, first_number, second_number)
+    right_answer = calculate(operator, first_number, second_number)
     question = f'{first_number} {operator} {second_number}'
 
     return question, str(right_answer)
